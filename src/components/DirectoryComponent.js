13dbdd4c094d48ff3b3b4edafa8b975027e963c3
Loren Zeman <Loren@lorencamille.net>
2021-12-17 19:27:34 -0500

    Added: campsites.js; Removed: array from DirectoryComponent.js

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -6,40 +6,6 @@ class Directory extends Component {
     this.state = {
       //this sets the state and must contain an object. you can create an empty object and come back later to fill it .
       //storing data for different campsites. becasue each campsite has different info associated with it, an array of objects is the best way to represent that.
-      campsites: [
-        {
-          id: 0,
-          name: "React Lake Campground",
-          image: "assets/images/react-lake.jpg",
-          elevation: 1233,
-          description:
-            "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
-        },
-        {
-          id: 1,
-          name: "Chrome River Campground ",
-          image: "assets/images/chrome-river.jpg",
-          elevation: 877,
-          description:
-            "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River.",
-        },
-        {
-          id: 2,
-          name: "Breadcrumb Trail Campground",
-          image: "assets/images/breadcrumb-trail.jpg",
-          elevation: 2901,
-          description:
-            "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground.",
-        },
-        {
-          id: 3,
-          name: "Redux Woods Campground",
-          image: "assets/images/redux-woods.jpg",
-          elevation: 42,
-          description:
-            "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
-        },
-      ],
     };
   }
   //   below you'll see a pair of curlies - this is how javascript is used in React, inside there is a variable called "directory"
